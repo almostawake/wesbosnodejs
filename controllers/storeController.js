6262a78b584ec815d24170c29cc68e2e3b2d4bf1
Andrew Walker <andrew@almostawake.com>
2020-04-25 10:57:25 +1000

    End 40, pagination working

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -46,8 +46,29 @@ exports.createStore = async (req, res) => {
 };
 
 exports.getStores = async (req, res) => {
-    const stores = await Store.find();
-    res.render('stores', { title: 'Stores', stores });
+    const page = req.params.page || 1;
+    const limit = 4;
+    const skip = (page * limit) - limit;
+
+    const storesPromise =  Store
+        .find()
+        .skip(skip)
+        .limit(limit)
+        .sort({ created: 'desc'});
+
+    const countPromise = Store.count();
+
+    const [stores, count] = await Promise.all([storesPromise, countPromise]);
+
+    const pages = Math.ceil(count / limit);
+
+    if(!stores.length && skip) {
+        req.flash('info',`No such page, you're on the last existing page`);
+        res.redirect(`/stores/page/${pages}`);
+        return;
+    }
+
+    res.render('stores', { title: 'Stores', stores, page, pages, count });
 };
 
 const confirmOwner = (store, user) => {
